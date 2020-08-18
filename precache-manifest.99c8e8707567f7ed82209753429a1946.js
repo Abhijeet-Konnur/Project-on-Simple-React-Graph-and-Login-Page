32a2127fa5c7e9583304ece8e62c8fc9dfc12026
1122Abhi <abhijeetkonnur@gmail.com>
2020-08-18 14:05:28 +0530

    Updates

---
+++ precache-manifest.99c8e8707567f7ed82209753429a1946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03764937e245c9bbe1ac7304905f569e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "557c11036374d86723ce",
+    "url": "/monsters-rolodex/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "89356da45280ac5f481b",
+    "url": "/monsters-rolodex/static/css/main.f52450e9.chunk.css"
+  },
+  {
+    "revision": "557c11036374d86723ce",
+    "url": "/monsters-rolodex/static/js/2.e76d9577.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e76d9577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89356da45280ac5f481b",
+    "url": "/monsters-rolodex/static/js/main.240bfe7e.chunk.js"
+  },
+  {
+    "revision": "bb668934ba86b57869a8",
+    "url": "/monsters-rolodex/static/js/runtime-main.89751c8a.js"
+  }
+]);
